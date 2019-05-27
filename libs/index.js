6196eaf034427facaa9a8fd8441a33ee2b63be95
enriqueviard <enrique.viard@innqube.com>
2019-05-26 21:36:55 -0300

    logHelper / eventEmitter

---
+++ libs/index.js
@@ -0,0 +1,4 @@
+exports.TwilioChatClient = require('./twilio-chat-client').default;
+exports.TwilioClient = require('./twilio-voice-client').default;
+exports.EventEmitterHelper = require('./event-emitter-helper').default;
+exports.LogHelper = require('./log-helper').default;
