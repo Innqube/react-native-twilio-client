be2619fe7b7039daaaffce29260673dd577f590a
diegofsza <diegofsza@gmail.com>
2019-09-13 17:30:48 -0300

    Added basic chat api implementation

--- android/src/main/java/com/ngs/react/RNTwilioClient/TwilioChatModule.java
+++ android/src/main/java/com/ngs/react/RNTwilioClient/TwilioChatModule.java
@@ -1,17 +1,19 @@
 package com.ngs.react.RNTwilioClient;
 
 import android.util.Log;
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.*;
+import com.ngs.react.Converters;
 import com.twilio.chat.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class TwilioChatModule extends ReactContextBaseJavaModule {
 
     private static final String LOG_TAG = "[IIMobile-Chat]";
     private ChatClient chatClient;
-    private CallbackListener chatCallbackListener;
 
     public TwilioChatModule(ReactApplicationContext context) {
         super(context);
@@ -31,20 +33,12 @@ public class TwilioChatModule extends ReactContextBaseJavaModule {
     public void create(String token, final Promise promise) {
         Log.d(LOG_TAG, "About to create client");
         ChatClient.Properties properties = new ChatClient.Properties.Builder().createProperties();
-        ChatClient.create(getReactApplicationContext(), token, properties, new CallbackListener<ChatClient>() {
+        ChatClient.create(getReactApplicationContext(), token, properties, new PromiseCallbackListener<ChatClient>(promise) {
             @Override
             public void onSuccess(ChatClient chatClient) {
                 Log.d(LOG_TAG, "Chat client created");
+                TwilioChatModule.this.chatClient = chatClient;
                 promise.resolve(null);
-            }
-
-            @Override
-            public void onError(ErrorInfo errorInfo) {
-                Log.d(LOG_TAG, "Error creating client: " + errorInfo.getCode() + ": " + errorInfo.getMessage());
-                promise.reject(
-                        Integer.valueOf(errorInfo.getCode()).toString(),
-                        errorInfo.getMessage()
-                );
             }
         });
     }
@@ -75,7 +69,7 @@ public class TwilioChatModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public Channels getChannels() {
+    public Channels getChannels(Promise promise) {
         return chatClient.getChannels();
     }
 
@@ -122,6 +116,142 @@ public class TwilioChatModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public boolean isReachabilityEnabled() {
         return chatClient.isReachabilityEnabled();
+    }
+
+
+    // Channels delegation
+    @ReactMethod
+    public void createChannel(String friendlyName, String uniqueName, ReadableMap attributes, final Promise promise) {
+        try {
+            JSONObject attr = Converters.convertMapToJson(attributes);
+            this.chatClient
+                    .getChannels()
+                    .channelBuilder()
+                    .withFriendlyName(friendlyName)
+                    .withUniqueName(uniqueName)
+                    .withAttributes(attr)
+                    .build(new PromiseCallbackListener<Channel>(promise) {
+                        @Override
+                        public void onSuccess(Channel channel) {
+                            try {
+                                JSONObject json = channelToJsonObject(channel);
+                                promise.resolve(Converters.convertJsonToMap(json));
+                            } catch (JSONException e) {
+                                promise.reject(e);
+                            }
+                        }
+                    });
+        } catch (JSONException e) {
+            promise.reject(e);
+        }
+    }
+
+    private JSONObject channelToJsonObject(Channel channel) throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("uniqueName", channel.getUniqueName());
+        json.put("friendlyName", channel.getFriendlyName());
+        json.put("sid", channel.getSid());
+        json.put("lastMessageIndex", channel.getLastMessageIndex());
+        json.put("attributes", channel.getAttributes());
+        return json;
+    }
+
+    private JSONObject messageToJsonObject(Message message) throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("author", message.getAuthor());
+        json.put("channelSid", message.getChannelSid());
+        json.put("messageBody", message.getMessageBody());
+        json.put("messageIndex", message.getMessageIndex());
+        json.put("sid", message.getSid());
+        json.put("attributes", message.getAttributes());
+        json.put("dateCreated", message.getDateCreated());
+        return json;
+    }
+
+    @ReactMethod
+    public void getChannel(String channelSidOrUniqueName, final Promise promise) {
+        chatClient.getChannels().getChannel(channelSidOrUniqueName, new PromiseCallbackListener<Channel>(promise) {
+            @Override
+            public void onSuccess(Channel channel) {
+                try {
+                    JSONObject json = channelToJsonObject(channel);
+                    promise.resolve(Converters.convertJsonToMap(json));
+                } catch (JSONException e) {
+                    promise.reject(e);
+                }
+            }
+        });
+    }
+
+    @ReactMethod
+    public void sendMessage(String channelSidOrUniqueName, final String message, final Promise promise) {
+        chatClient
+                .getChannels()
+                .getChannel(channelSidOrUniqueName, new PromiseCallbackListener<Channel>(promise) {
+                    @Override
+                    public void onSuccess(Channel channel) {
+                        channel
+                                .getMessages()
+                                .sendMessage(
+                                        Message.options().withBody(message),
+                                        new PromiseCallbackListener<Message>(promise) {
+                                            @Override
+                                            public void onSuccess(Message message) {
+                                                try {
+                                                    JSONObject json = messageToJsonObject(message);
+                                                    promise.resolve(json);
+                                                } catch (JSONException e) {
+                                                    promise.reject(e);
+                                                }
+                                            }
+                                        }
+                                );
+                    }
+                });
+    }
+
+    @ReactMethod
+    public void getMessages(String channelSidOrUniqueName, final Long index, final Integer count, final Promise promise) {
+        chatClient
+                .getChannels()
+                .getChannel(channelSidOrUniqueName, new PromiseCallbackListener<Channel>(promise) {
+                    @Override
+                    public void onSuccess(Channel channel) {
+                        channel.getMessages()
+                                .getMessagesAfter(index, count, new PromiseCallbackListener<List<Message>>(promise) {
+                                    @Override
+                                    public void onSuccess(List<Message> messages) {
+                                        try {
+                                            JSONArray jsonArray = new JSONArray();
+
+                                            for (Message message : messages) {
+                                                jsonArray.put(messageToJsonObject(message));
+                                            }
+
+                                            promise.resolve(Converters.convertJsonToArray(jsonArray));
+                                        } catch (JSONException je) {
+                                            promise.resolve(je);
+                                        }
+                                    }
+                                });
+                    }
+                });
+    }
+
+    abstract class PromiseCallbackListener<T> extends CallbackListener<T> {
+
+        private Promise promise;
+
+        PromiseCallbackListener(Promise promise) {
+            this.promise = promise;
+        }
+
+        @Override
+        public void onError(ErrorInfo errorInfo) {
+            Log.d(LOG_TAG, "Error creating client: " + errorInfo.getCode() + ": " + errorInfo.getMessage());
+            promise.reject(Integer.valueOf(errorInfo.getCode()).toString(), errorInfo.getMessage());
+        }
+
     }
 
 }
