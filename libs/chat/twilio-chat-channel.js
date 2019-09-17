af90bab11d517d6206ea360d021a012d84e2c1e1
enriqueviard <enrique.viard@innqube.com>
2019-09-17 11:42:56 -0300

    client api

--- libs/chat/twilio-chat-channel.js
+++ libs/chat/twilio-chat-channel.js
@@ -7,8 +7,6 @@ import {NativeModules} from 'react-native';
 
 const {
     RNTwilioChatChannel,
-    RNTwilioChatMessage,
-    RNTwilioChatMember
 } = NativeModules;
 
 const TwilioChatChannel = (props) => {
@@ -22,24 +20,38 @@ const TwilioChatChannel = (props) => {
     this.onNewMessage = (message) => dispatchEvent(new CustomEvent('onNewMessage', message));
 
     this.join = (uniqueName, friendlyName, type) => RNTwilioChatChannel.join(uniqueName, friendlyName, type);
+
     this.leave = () => RNTwilioChatChannel.leave();
+
     this.typing = () => RNTwilioChatChannel.typing();
+
     this.getUnreadMessagesCount = () => RNTwilioChatChannel.getUnreadMessagesCount();
+
     this.getMessagesCount = () => RNTwilioChatChannel.getMessagesCount();
+
     this.getMembersCount = () => RNTwilioChatChannel.getMembersCount();
 
-    this.getLastMessages = (count = 10) =>  RNTwilioChatMessage.getLastMessages(count);
-    this.getMessages = (index, count) => RNTwilioChatMessage.getMessages(this.uniqueName, index, count);
-    this.getMessagesBefore = (index, count) => RNTwilioChatMessage.getMessagesBefore(index, count);
-    this.getMessagesAfter = (index, count) => RNTwilioChatMessage.getMessagesAfter(index, count);
-    this.setNoMessagesConsumed = () => RNTwilioChatMessage.setNoMessagesConsumed();
-    this.setAllMessagesConsumed = () => RNTwilioChatMessage.setAllMessagesConsumed();
-    this.setLastConsumedMessage = (index) => RNTwilioChatMessage.setLastConsumedMessage(index);
-    this.advanceLastConsumedMessage = (index) => RNTwilioChatMessage.advanceLastConsumedMessage(index);
-    this.getLastConsumedMessageIndex = () => RNTwilioChatMessage.getLastConsumedMessageIndex();
-    this.sendMessage = (message) =>  RNTwilioChatMessage.sendMessage(message);
+    this.getLastMessages = (count = 10) =>  RNTwilioChatChannel.getLastMessages(count);
 
-    this.getChannelMembers = () => RNTwilioChatMember.getChannelMembers();
+    this.getMessages = (index, count) => RNTwilioChatChannel.getMessages(this.uniqueName, index, count);
+
+    this.getMessagesBefore = (index, count) => RNTwilioChatChannel.getMessagesBefore(index, count);
+
+    this.getMessagesAfter = (index, count) => RNTwilioChatChannel.getMessagesAfter(index, count);
+
+    this.setNoMessagesConsumed = () => RNTwilioChatChannel.setNoMessagesConsumed();
+
+    this.setAllMessagesConsumed = () => RNTwilioChatChannel.setAllMessagesConsumed();
+
+    this.setLastConsumedMessage = (index) => RNTwilioChatChannel.setLastConsumedMessage(index);
+
+    this.advanceLastConsumedMessage = (index) => RNTwilioChatChannel.advanceLastConsumedMessage(index);
+
+    this.getLastConsumedMessageIndex = () => RNTwilioChatChannel.getLastConsumedMessageIndex();
+
+    this.sendMessage = (message) =>  RNTwilioChatChannel.sendMessage(message);
+
+    this.getChannelMembers = () => RNTwilioChatChannel.getChannelMembers();
 };
 
 export default TwilioChatChannel;
