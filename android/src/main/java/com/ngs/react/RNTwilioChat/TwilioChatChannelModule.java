7aba0a4a85095053d319465ea073386faa1017e9
diegofsza <diegofsza@gmail.com>
2019-09-18 17:25:27 -0300

    Moved event listener

--- android/src/main/java/com/ngs/react/RNTwilioChat/TwilioChatChannelModule.java
+++ android/src/main/java/com/ngs/react/RNTwilioChat/TwilioChatChannelModule.java
@@ -9,11 +9,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TwilioChatChannelModule extends ReactContextBaseJavaModule {
+public class TwilioChatChannelModule extends ReactContextBaseJavaModule implements ChannelListener {
 
     private static final String LOG_TAG = "[IIMobile-Channel]";
+    private List<String> channelListeners = new ArrayList<>();
 
     public TwilioChatChannelModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -26,6 +28,13 @@ public class TwilioChatChannelModule extends ReactContextBaseJavaModule {
 
     interface GotChannel {
         void gotChannel(Channel channel);
+    }
+
+    private void addListener(Channel channel) {
+        if (!channelListeners.contains(channel.getSid())) {
+            channel.addListener(this);
+            channelListeners.add(channel.getSid());
+        }
     }
 
     private void getChannel(String channelSidOrUniqueName, final GotChannel gotChannel) {
@@ -56,6 +65,7 @@ public class TwilioChatChannelModule extends ReactContextBaseJavaModule {
                 .getChannel(channelSidOrUniqueName, new PromiseCallbackListener<Channel>(promise) {
                     @Override
                     public void onSuccess(Channel channel) {
+                        addListener(channel);
                         try {
                             JSONObject json = Utils.channelToJsonObject(channel);
                             promise.resolve(Utils.convertJsonToMap(json));
@@ -84,7 +94,6 @@ public class TwilioChatChannelModule extends ReactContextBaseJavaModule {
                         public void onSuccess(Channel channel) {
                             try {
                                 channel.removeAllListeners();
-                                channel.addListener(new TwilioChannelListener(getReactApplicationContext()));
                                 JSONObject json = Utils.channelToJsonObject(channel);
                                 promise.resolve(Utils.convertJsonToMap(json));
                             } catch (JSONException e) {
@@ -104,6 +113,7 @@ public class TwilioChatChannelModule extends ReactContextBaseJavaModule {
             channel.join(new StatusListener() {
                 @Override
                 public void onSuccess() {
+                    addListener(channel);
                     promise.resolve(null);
                 }
 
@@ -112,7 +122,6 @@ public class TwilioChatChannelModule extends ReactContextBaseJavaModule {
                     promise.reject(Integer.valueOf(errorInfo.getCode()).toString(), errorInfo.getMessage());
                 }
             });
-            channel.addListener(new TwilioChannelListener(getReactApplicationContext()));
         });
     }
 
@@ -123,6 +132,7 @@ public class TwilioChatChannelModule extends ReactContextBaseJavaModule {
             channel.leave(new StatusListener() {
                 @Override
                 public void onSuccess() {
+                    channelListeners.remove(channel.getSid());
                     promise.resolve(null);
                 }
 
@@ -312,4 +322,160 @@ public class TwilioChatChannelModule extends ReactContextBaseJavaModule {
 
     }
 
+
+    // Channel listener implementation
+    @Override
+    public void onMessageAdded(Message message) {
+        Log.d(LOG_TAG, "onMessageAdded: " + message.getSid());
+        try {
+            WritableMap wrapper = new WritableNativeMap();
+
+            JSONObject messageJson = Utils.messageToJsonObject(message);
+            WritableMap messageMap = Utils.convertJsonToMap(messageJson);
+
+            wrapper.putString("channelSid", message.getChannelSid());
+            wrapper.putMap("message", messageMap);
+
+            Utils.sendEvent(getReactApplicationContext() ,"messageAdded", wrapper);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Could not handle event", e);
+        }
+    }
+
+    @Override
+    public void onMessageUpdated(Message message, Message.UpdateReason updateReason) {
+        Log.d(LOG_TAG, "onMessageUpdated: " + message.getSid());
+        try {
+            WritableMap wrapper = new WritableNativeMap();
+
+            JSONObject messageJson = Utils.messageToJsonObject(message);
+            WritableMap messageMap = Utils.convertJsonToMap(messageJson);
+
+            wrapper.putString("channelSid", message.getChannelSid());
+            wrapper.putString("reason", updateReason.name());
+            wrapper.putMap("message", messageMap);
+
+            Utils.sendEvent(getReactApplicationContext() ,"messageUpdated", wrapper);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Could not handle event", e);
+        }
+    }
+
+    @Override
+    public void onMessageDeleted(Message message) {
+        Log.d(LOG_TAG, "onMessageDeleted: " + message.getSid());
+        try {
+            WritableMap wrapper = new WritableNativeMap();
+
+            JSONObject messageJson = Utils.messageToJsonObject(message);
+            WritableMap messageMap = Utils.convertJsonToMap(messageJson);
+
+            wrapper.putString("channelSid", message.getChannelSid());
+            wrapper.putMap("message", messageMap);
+
+            Utils.sendEvent(getReactApplicationContext() ,"messageDeleted", wrapper);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Could not handle event", e);
+        }
+    }
+
+    @Override
+    public void onMemberAdded(Member member) {
+        Log.d(LOG_TAG, "onMemberAdded: " + member.getSid());
+        try {
+            WritableMap wrapper = new WritableNativeMap();
+            JSONObject memberJson = Utils.memberToJsonObject(member);
+
+            WritableMap memberMap = Utils.convertJsonToMap(memberJson);
+
+            wrapper.putString("channelSid", member.getChannel().getSid());
+            wrapper.putMap("member", memberMap);
+
+            Utils.sendEvent(getReactApplicationContext() ,"memberAdded", wrapper);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Could not handle event", e);
+        }
+    }
+
+    @Override
+    public void onMemberUpdated(Member member, Member.UpdateReason updateReason) {
+        Log.d(LOG_TAG, "onMemberUpdated: " + member.getSid());
+        try {
+            WritableMap wrapper = new WritableNativeMap();
+            JSONObject memberJson = Utils.memberToJsonObject(member);
+
+            WritableMap memberMap = Utils.convertJsonToMap(memberJson);
+
+            wrapper.putString("channelSid", member.getChannel().getSid());
+            wrapper.putMap("member", memberMap);
+            wrapper.putString("reason", updateReason.name());
+
+            Utils.sendEvent(getReactApplicationContext() ,"memberUpdated", wrapper);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Could not handle event", e);
+        }
+    }
+
+    @Override
+    public void onMemberDeleted(Member member) {
+        Log.d(LOG_TAG, "onMemberDeleted: " + member.getSid());
+        try {
+            WritableMap wrapper = new WritableNativeMap();
+            JSONObject memberJson = Utils.memberToJsonObject(member);
+
+            WritableMap memberMap = Utils.convertJsonToMap(memberJson);
+
+            wrapper.putString("channelSid", member.getChannel().getSid());
+            wrapper.putMap("member", memberMap);
+
+            Utils.sendEvent(getReactApplicationContext() ,"memberDeleted", wrapper);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Could not handle event", e);
+        }
+    }
+
+    @Override
+    public void onTypingStarted(Channel channel, Member member) {
+        Log.d(LOG_TAG, "onTypingStarted: " + member.getSid());
+        try {
+            WritableMap wrapper = new WritableNativeMap();
+            JSONObject memberJson = Utils.memberToJsonObject(member);
+
+            WritableMap memberMap = Utils.convertJsonToMap(memberJson);
+
+            wrapper.putString("channelSid", channel.getSid());
+            wrapper.putMap("member", memberMap);
+
+            Utils.sendEvent(getReactApplicationContext() ,"typingStarted", wrapper);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Could not handle event", e);
+        }
+    }
+
+    @Override
+    public void onTypingEnded(Channel channel, Member member) {
+        Log.d(LOG_TAG, "onTypingEnded: " + member.getSid());
+        try {
+            WritableMap wrapper = new WritableNativeMap();
+            JSONObject memberJson = Utils.memberToJsonObject(member);
+
+            WritableMap memberMap = Utils.convertJsonToMap(memberJson);
+
+            wrapper.putString("channelSid", channel.getSid());
+            wrapper.putMap("member", memberMap);
+
+            Utils.sendEvent(getReactApplicationContext() ,"typingEnded", wrapper);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Could not handle event", e);
+        }
+    }
+
+    @Override
+    public void onSynchronizationChanged(Channel channel) {
+        Log.d(LOG_TAG, "onSynchronizationChanged");
+        WritableMap wrapper = new WritableNativeMap();
+        wrapper.putString("channelSid", channel.getSid());
+        wrapper.putString("status", channel.getStatus().name());
+        Utils.sendEvent(getReactApplicationContext() ,"channelSynchronizationStatusUpdated", wrapper);
+    }
 }
