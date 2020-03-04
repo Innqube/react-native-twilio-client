e5b59425ec03eeca143709c4af6234e4981dbfca
enriqueviard <enrique.viard@innqube.com>
2020-03-04 11:01:49 -0300

    timestamp serialization

--- ios/RNTwilioClient/RCTConvert+TwilioChatClient.m
+++ ios/RNTwilioClient/RCTConvert+TwilioChatClient.m
@@ -237,10 +237,8 @@ RCT_ENUM_CONVERTER(TCHClientConnectionState,(@{
            @"index": message.index,
            @"author": RCTNullIfNil(message.author),
            @"body": RCTNullIfNil(message.body),
-           @"timestamp": RCTNullIfNil(message.timestamp),
-           @"timestampAsDate": message.timestampAsDate == nil ? @"null" : @(message.timestampAsDate.timeIntervalSince1970 * 1000),
-           @"dateUpdated": RCTNullIfNil(message.dateUpdated),
-           @"dateUpdatedDate": message.dateUpdatedAsDate == nil ? @"null" : @(message.dateUpdatedAsDate.timeIntervalSince1970 * 1000),
+           @"timestamp": message.timestampAsDate == nil ? @"null" : @(message.timestampAsDate.timeIntervalSince1970 * 1000),
+           @"dateUpdated": message.dateUpdatedAsDate == nil ? @"null" : @(message.dateUpdatedAsDate.timeIntervalSince1970 * 1000),
            @"lastUpdatedBy": RCTNullIfNil(message.lastUpdatedBy),
            @"attributes": RCTNullIfNil(message.attributes)
            };
